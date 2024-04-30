860b9e95b44c67fab13d152439ea11853f830c54
giovanaluu <114152057+giovanaluu@users.noreply.github.com>
2024-04-30 07:33:35 -0300

    .

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ for(let i= 0;i<botoes.length;i++){
         }
 
 
-        botoes.[i].classList.add('ativo')
-        textos.[i].classList.add('ativo')
+        botoes[i].classList.add('ativo')
+        textos[i].classList.add('ativo')
        
 }
 }
